bb38c52f2d62eba5602be2982ed3e9b53f1f2306
kusnandartoni <kusnandartoni@gmail.com>
2018-02-25 12:09:48 +0700

    add pages

---
+++ src/pages/aturan/aturan.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AturanPage } from './aturan';
+
+@NgModule({
+  declarations: [
+    AturanPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AturanPage),
+  ],
+})
+export class AturanPageModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,14 +17,14 @@ export class HomePage {
 
   user = {
     name: 'Toni Kusnandar',
-    profileImage: 'assets/img/avatar/girl-avatar.png',
+    profileImage: 'assets/img/avatar/pp.jpg',
     coverImage: 'assets/img/background/background-5.jpg',
     occupation: 'Designer',
     location: 'Seattle, WA',
     description: 'A wise man once said: The more you do something, the better you will become at it.',
-    followers: 456,
-    following: 1052,
-    posts: 35
+    followers: 0,
+    following: 0,
+    posts: 0
   };
   posts = [
     {
